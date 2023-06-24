6ebe67727a54a8a39cc1e7ed5807d144c891b661
fdhliakbar <fadhliakbar125@gmail.com>
2023-06-25 00:24:21 +0700

    Edit file matrix

--- Algorithms/input-data/matrix.php
+++ Algorithms/input-data/matrix.php
@@ -1,18 +1,13 @@
 <?php
-// Step 1: Tentukan array 2 dimensi yang ingin ditambahkan data baru
-$myArray = array(
+$matrix = array(
     array("Nama", "Usia", "Kota"),
-    array("John", 25, "Jakarta"),
-    array("Jane", 30, "Surabaya"),
-    array("Bob", 35, "Bandung")
+    array("fdhli", 21, "jogja"),
+    array("kamu", 15, "bantul"),
+    array("dia", 35, "bali")
 );
 
-// Step 2: Buat variabel untuk menyimpan data baru yang ingin ditambahkan ke dalam array
 $newData = array("Mike", 28, "Bali");
 
-// Step 3: Tambahkan data baru ke dalam variabel menggunakan sintaksis array_push()
-array_push($myArray, $newData);
-
-// Step 4: Tambahkan variabel dengan data baru ke dalam array menggunakan sintaksis array_push() lagi
-print_r($myArray);
+array_push($matrix, $newData);
+print_r($matrix);
 ?>
