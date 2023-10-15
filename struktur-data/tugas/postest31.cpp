f6708e50c6b487547dbcd564fe7e64a572391af4
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-10-15 19:21:00 +0700

    upload: latihan struktur data

---
+++ struktur-data/tugas/postest31.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,n;
+
+    cout << "Masukkan jumlah item: " << '\n';
+    cin >> n;
+
+    int *arr = new int(n);
+
+    cout << "Masukkan " << n << " item" << endl;
+
+    for (x = 0; x< n; x++) {
+        cin >> arr[x];
+    }
+    cout << "Output: ";
+    for (x = 0; x < n; x++) {
+        cout << arr[x] << " ";
+    }
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int reverse(int value) {
+    while(0 < value) {
+        
+    }
+}
+
+int main(){
+    int value;
+
+    cout << "Masukkan angka: ";
+    cin >> value;
+
+    cout << "Output: ";
+}
