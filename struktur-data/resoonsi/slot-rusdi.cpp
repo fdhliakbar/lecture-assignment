3a45db10db0b853c65ea1312c1924bc88a26548a
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2024-01-01 10:08:36 +0700

    docs: create a new class(Tree)

---
+++ struktur-data/resoonsi/slot-rusdi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Node = 10,2,3,4,5,2,1
+    jalankan fungsi : inorder,preorder,postorder
+    dan cari node yang dipilih misal, 40
+*/
+
+
+class Tree {
+    public:
+    void findNode(int keyValue) {
+        cout << "halo";
+    }
+};
+
+
+
+int main() {
+    Tree TREE;
+
+    TREE.findNode(10);
+    return 0;
+}
