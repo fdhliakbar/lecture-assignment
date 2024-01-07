3ad0c2d5ead40c25d01cf338c348739f83c1f0bb
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2024-01-07 09:35:24 +0700

    docs: Update in the main function

--- struktur-data/resoonsi/slot-rusdi.cpp
+++ struktur-data/resoonsi/slot-rusdi.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
-int main() {
+
+struct Node {
+    int right;
+    int left;
+};
+
+class Node {
+    public:
+        void Leap() {
+
+        }
+};
+
+
+int main(){
+    Node node;
+
+    std::cout << "RESPONSI - 2200018197" << std::endl;
+    std::cout << "Masukkan Nilai Untuk Tree" << '\n';
+
+
     return 0;
 }
